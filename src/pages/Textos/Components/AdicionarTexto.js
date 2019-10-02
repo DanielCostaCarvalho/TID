752457483e44982a322cc905755070ba2b88e2e8
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2019-10-02 07:57:34 -0300

    Envia texto cadastrado e recebe na listagem

--- src/pages/Textos/Components/AdicionarTexto.js
+++ src/pages/Textos/Components/AdicionarTexto.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import { Text, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { Button } from 'react-native';
 
-// import { Container } from './styles';
+import Texto from '../../../models/Texto';
+
+import { Container, InputTitle, Title, InputText } from './styles';
 
 export default function AdicionarTexto() {
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState(false);
+
+  const handleAddText = () => {
+    Texto.create({ titulo: title, texto: text });
+  };
+
   return (
-    <>
-      <Text>Título</Text>
-      <TextInput>Oi</TextInput>
-    </>
+    <Container>
+      <Title>Título</Title>
+      <InputTitle
+        placeholder="Título"
+        value={title}
+        onChangeText={titulo => setTitle(titulo)}
+        returnKeyType="next"
+      />
+      <Title>Texto</Title>
+      <InputText
+        placeholder="Texto"
+        returnKeyType="send"
+        onSubmitEditing={handleAddText}
+        onChangeText={texto => setText(texto)}
+        value={text}
+      />
+      <Button title="Enviar" onPress={handleAddText} />
+    </Container>
   );
 }
 
--- src/pages/Textos/Components/ListaTexto.js
+++ src/pages/Textos/Components/ListaTexto.js
@@ -2,25 +2,32 @@ import React from 'react';
 import { FlatList, Text, Button } from 'react-native';
 
 import Texto from '../../../models/Texto';
-// import { Container } from './styles';
+import { List, Container, Title, Excerpt, TextButton } from './styles';
 
 const dados = Texto.findAll();
 
 export default function ListaTexto({ navigation }) {
   return (
-    <>
+    <Container>
       <Button
         onPress={() => {
           navigation.navigate('Adicionar');
         }}
         title="Adicionar"
       />
-      <FlatList
+      <List
         data={dados}
         keyExtractor={dado => String(dado.titulo)}
-        renderItem={({ item }) => <Text>{item.titulo}</Text>}
+        renderItem={({ item }) => {
+          return (
+            <TextButton>
+              <Title>{item.titulo}</Title>
+              <Excerpt>{item.texto}</Excerpt>
+            </TextButton>
+          );
+        }}
       />
-    </>
+    </Container>
   );
 }
 
