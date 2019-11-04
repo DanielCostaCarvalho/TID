5a1f8d7c6131b952900b4e929512c33243f92458
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2019-11-03 21:08:01 -0300

    Começando a parte de SRS

--- src/pages/SRS/index.js
+++ src/pages/SRS/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
+
+import { Expressoes } from '../../models';
 
 // import { Container } from './styles';
 
 export default function SRS() {
+  const [dados, setDados] = useState([]);
+  const [buscar, setBuscar] = useState(false);
+
+  Expressoes.onChange(() => setBuscar(true));
+
+  useEffect(() => {
+    async function fetchData() {
+      const consulta = await Expressoes.data();
+      const treino = consulta.filter(expressao => {
+        return expressao.proxtreino <= new Date();
+      });
+      setDados(treino);
+      console.log(treino);
+    }
+    fetchData();
+    console.log(dados);
+    setBuscar(false);
+  }, [buscar]);
+
   return (
     <View>
       <Text>Página de Repetição espaçada</Text>
