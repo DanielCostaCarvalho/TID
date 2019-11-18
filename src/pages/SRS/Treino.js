224925c9597a5ca611e221afb10c9770d6532152
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2019-11-17 23:35:22 -0300

    Iniciando sistema de repetição espaçada

--- src/pages/SRS/Treino.js
+++ src/pages/SRS/Treino.js
@@ -1,31 +1,153 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { Title, Text, Button } from 'react-native-paper';
 
 import { Expressoes } from '../../models';
 
-// import { Container } from './styles';
+import { Container, ContainerTreino, BotoesTreino } from './styles';
 
 export default function SRS() {
-  const [dados, setDados] = useState([]);
+  const [palavra, setPalavra] = useState('');
   const [buscar, setBuscar] = useState(false);
+  const [respondido, setRespondido] = useState(false);
+  const [treinado, setTreinado] = useState(false);
 
-  Expressoes.onChange(() => setBuscar(true));
+  const handleAcertou = () => {
+    const novoTreino = new Date();
+    let caixa = palavra.nivel;
+    switch (caixa) {
+      case '0':
+        novoTreino.setDate(new Date().getDate() + 10);
+        caixa = '1';
+        break;
+      case '1':
+        novoTreino.setDate(new Date().getDate() + 10);
+        caixa = '2';
+        break;
+      case '2':
+        novoTreino.setDate(new Date().getDate() + 10);
+        caixa = '3';
+        break;
+      case '3':
+        novoTreino.setDate(new Date().getDate() + 10);
+        caixa = '4';
+        break;
+      case '4':
+        novoTreino.setDate(new Date().getDate() + 10);
+        caixa = '5';
+        break;
+      case '5':
+        novoTreino.setDate(new Date().getDate() + 10);
+        caixa = '5';
+        break;
+      default:
+        break;
+    }
+    Expressoes.update(palavra.id, { proxtreino: novoTreino, nivel: caixa });
+
+    setRespondido(false);
+    setBuscar(true);
+  };
+
+  const handleErrou = () => {
+    const novoTreino = new Date();
+    let caixa = palavra.nivel;
+    switch (caixa) {
+      case '0':
+        novoTreino.setDate(new Date().getDate() + 10);
+        caixa = '1';
+        break;
+      case '1':
+        novoTreino.setDate(new Date().getDate() + 10);
+        caixa = '2';
+        break;
+      case '2':
+        novoTreino.setDate(new Date().getDate() + 10);
+        caixa = '3';
+        break;
+      case '3':
+        novoTreino.setDate(new Date().getDate() + 10);
+        caixa = '4';
+        break;
+      case '4':
+        novoTreino.setDate(new Date().getDate() + 10);
+        caixa = '5';
+        break;
+      case '5':
+        novoTreino.setDate(new Date().getDate() + 10);
+        caixa = '5';
+        break;
+      default:
+        break;
+    }
+    Expressoes.update(palavra.id, { proxtreino: novoTreino, nivel: caixa });
+
+    setRespondido(false);
+    setBuscar(true);
+  };
 
   useEffect(() => {
     async function fetchData() {
       const consulta = await Expressoes.data().filter(expressao => {
         return expressao.proxtreino <= new Date();
       });
-      setDados(consulta);
+      if (consulta.length == 0) {
+        setTreinado(true);
+      }
+      setPalavra(consulta[0]);
     }
     fetchData();
-    console.log(dados);
     setBuscar(false);
   }, [buscar]);
 
   return (
-    <View>
-      <Text>Página de Repetição espaçada</Text>
-    </View>
+    <>
+      {treinado ? (
+        <Container>
+          <Text>Parabens, você já treinou todas as palavras!</Text>
+        </Container>
+      ) : (
+        <ContainerTreino>
+          <Title>{palavra.expressao}</Title>
+          {respondido ? (
+            <>
+              <Text>{palavra.traducao}</Text>
+              <BotoesTreino>
+                <Button
+                  mode="outlined"
+                  onPress={() => handleErrou()}
+                  icon="window-close"
+                  color="#b00"
+                >
+                  Errou
+                </Button>
+                <Button
+                  mode="outlined"
+                  onPress={() => handleAcertou()}
+                  icon="check"
+                  color="#0b0"
+                >
+                  Acertou
+                </Button>
+              </BotoesTreino>
+            </>
+          ) : (
+            <>
+              <Text />
+
+              <Button
+                mode="outlined"
+                onPress={() => {
+                  setRespondido(true);
+                }}
+                icon="translate"
+                color="#333"
+              >
+                Mostrar tradução
+              </Button>
+            </>
+          )}
+        </ContainerTreino>
+      )}
+    </>
   );
 }
--- src/pages/SRS/styles.js
+++ src/pages/SRS/styles.js
@@ -3,6 +3,20 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   padding: 10px;
-  justify-content: flex-end;
+  justify-content: center;
   align-items: center;
+`;
+
+export const ContainerTreino = styled.View`
+  flex: 1;
+  padding: 10px;
+  align-items: center;
+`;
+
+export const BotoesTreino = styled.View`
+  padding: 10px;
+  flex-direction: row;
+  align-items: flex-end;
+  align-self: center;
+  justify-content: space-between;
 `;
